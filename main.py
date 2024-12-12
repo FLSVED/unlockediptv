1cae1c33e22742f2065be2112d4772c801e0c569
FLSVED <kamel.labiad.uk1@gmail.com>
2024-12-12 10:21:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -23,3 +23,5 @@ app.include_router(router, prefix="/api/v1")
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+    input("Appuyez sur Entrée pour fermer...")
+
